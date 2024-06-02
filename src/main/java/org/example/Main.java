9e78320c7470a09c3d3f38c588b090f8e1a3675e
giovvbatista <giovvbatista@gmail.com>
2024-06-01 21:13:37 -0300

    added some test cases

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,14 +7,18 @@ import org.jgrapht.graph.SimpleGraph;
 import java.util.List;
 import java.util.Set;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Graph<Integer, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
-        String filePath = "/assets/teste2.txt";
+
+        Scanner scanner = new Scanner(System.in);
+        String filePath = scanner.nextLine();
 
         ReadGraph.read(filePath, graph);
 
-        int desiredCommunities = 3;
+        int desiredCommunities = 8;
         List<Set<Integer>> communities = GirvanNewman.split(graph, desiredCommunities);
 
         System.out.println("Communities found:");
